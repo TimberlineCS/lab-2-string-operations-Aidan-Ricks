57f44cff08664b2932b6e61a9f7125e93b029d68
Aidan <aidanjricks@gmail.com>
2020-09-02 16:45:30 +0000

    completed code

---
+++ newlab.java
@@ -0,0 +1,18 @@
+public class Wordscrabler{
+  public static void main(String args[]){
+
+    //Chaleng 1
+  String word = "hello";
+  int wordLength = word.length();
+  int midPoint = word.length()/2;
+  String firsthalf = word.substring(0,midPoint);
+  String secondhalf = word.substring(word.length()/2);
+  String mixed = secondhalf + firsthalf;
+  System.out.print(mixed);
+  // Chaleng 2
+  System.out.println(" \n     ________    \n    /_______/\\    \n    \\ \\    / /\n ____\\ \\__/_/____\n/_____\\ \\_______/\\\n\\ \\    \\/ /    / /\n \\ \\   / /\\   / /\n  \\ \\ / /\\ \\ / /\n   \\_\\_/  \\_\\_/");
+  }
+
+
+
+}
